4b470e65ae38d911fab646b1d65b5c307dca3f6a
Ron Alter <ronalter@gmail.com>
2013-12-01 21:42:41 +0200

    just some hebrew..

--- language/he/acp/attachments.php
+++ language/he/acp/attachments.php
@@ -32,7 +32,7 @@ if (empty($lang) || !is_array($lang))
 // translators to re-order the output of data while ensuring it remains correct
 //
 // You do not need this where single placeholders are used, e.g. 'Message %d' is fine
-// equally where a string contains only two placeholders whic~h are used to wrap text
+// equally where a string contains only two placeholders which are used to wrap text
 // in a url you again do not need to specify an order e.g., 'Click %sHERE%s' is fine
 
 $lang = array_merge($lang, array(
