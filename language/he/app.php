b538c0d543830b61c97d6248d9a97d977a97bad4
Ronny Orbach <ronny@reallygood.co.il>
2014-10-30 11:45:05 +0200

    app.php translations, pretty low level and sounds bad in Hebrew, but ok

--- language/he/app.php
+++ language/he/app.php
@@ -41,8 +41,9 @@ if (empty($lang) || !is_array($lang))
 //
 
 $lang = array_merge($lang, array(
-	'CONTROLLER_ARGUMENT_VALUE_MISSING'	=> 'Missing value for argument #%1$s: <strong>%3$s</strong> in class <strong>%2$s</strong>',
-	'CONTROLLER_NOT_SPECIFIED'			=> 'No controller has been specified.',
-	'CONTROLLER_METHOD_NOT_SPECIFIED'	=> 'No method was specified for the controller.',
+	'CONTROLLER_ARGUMENT_VALUE_MISSING'	=> 'חסר ערך לפרמטר #%1$s: <strong>%3$s</strong> במחלקה <strong>%2$s</strong>',
+	'CONTROLLER_NOT_SPECIFIED'			=> 'לא הוגדר Controller',
+	'CONTROLLER_METHOD_NOT_SPECIFIED'	=> 'לא הוגדרה מתודה ל-Controller',
 	'CONTROLLER_SERVICE_UNDEFINED'		=> 'The service for controller "<strong>%s</strong>" is not defined in ./config/services.yml.',
+	'CONTROLLER_SERVICE_UNDEFINED'		=> 'השירות עבור ה-Controller "<strong>%s</strong>" לא מוגדר בקובץ ./config/services.yml.',
 ));
